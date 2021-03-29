4bb3e318a87900851264e581a035a56ba2aff90e
Javier <https://github.com/JavierMarSan18>
2021-03-28 21:27:00 -0600

    First commit

---
+++ Estudiante.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ListaEstudiantes
+{
+    class Estudiante
+    {
+        public string nombre;
+		public int edad;
+		public string carnet;
+		public bool solvente;
+		public string[] curso;
+		
+		public Estudiante(string nombre, int edad, string carnet, bool solvente, string[] curso)
+		{
+			this.nombre = nombre;
+			this.edad = edad;
+			this.carnet = carnet;
+			this.solvente = solvente;
+			this.curso = curso;
+		}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace ListaEstudiantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+			{
+				Estudiante[] listaEstudiantes = new Estudiante[2];
+				string[] curso;
+				int cont = 0;
+				
+				while(cont < 2)
+				{
+					//Informacion personal
+					string nombre;
+					int edad;
+					string carnet;
+					bool solvente;
+					
+					//Informacion del curso
+					string nombreCurso;
+					string nota_1;
+					string nota_2;
+					string notaFinal;
+					
+					Console.WriteLine($"\nESTUDIANTE {cont + 1}");
+					Console.WriteLine("\nInformación personal:");
+					
+					Console.WriteLine("\nIngrese nombre");
+					nombre = Console.ReadLine();
+					
+					Console.WriteLine("\nIngrese edad");
+					edad = Convert.ToInt32(Console.ReadLine());
+					
+					Console.WriteLine("\nIngrese carnet");
+					carnet = Console.ReadLine();
+					
+					Console.WriteLine("\nIngrese si esta solvente");
+					solvente = Convert.ToBoolean(Console.ReadLine());
+					
+
+					Console.WriteLine("\n\nInformación del curso:");
+					
+					Console.WriteLine("\nIngrese nombre");
+					nombreCurso = Console.ReadLine();
+					
+					Console.WriteLine("\nIngrese nota del parcial 1");
+					nota_1 = Console.ReadLine();
+					
+					Console.WriteLine("\nIngrese nota del parcial 2");
+					nota_2 = Console.ReadLine();
+					
+					Console.WriteLine("\nIngrese nota del final");
+					notaFinal = Console.ReadLine();
+					
+					curso = new string[4]{nombreCurso, nota_1, nota_2, notaFinal};
+				
+					Estudiante estudiante = new Estudiante(nombre, edad, carnet, solvente, curso);
+					
+					listaEstudiantes[cont] = estudiante; 
+					
+					cont++;
+					Console.Clear();
+				}
+				
+				Console.WriteLine("\nLISTA DE ESTUDIANTES\n");
+				Console.WriteLine("-----------------------------------------------------------");
+				
+				for(int i = 0 ; i < listaEstudiantes.Length ; i++)
+				{
+					Estudiante estudiante = listaEstudiantes[i];
+					
+					//Personal
+					Console.WriteLine("Informacion Personal:");
+					Console.WriteLine($"Nombre: {estudiante.nombre}\nCarnet: {estudiante.carnet}\nEdad: {estudiante.edad}\nSolvente: {estudiante.solvente}");
+					
+					//Curso
+					Console.WriteLine("\n\tInformacion del Curso:");
+					Console.WriteLine($"\tNombre curso: {estudiante.curso[0]}\n\tNota parcial 1: {estudiante.curso[1]} \n\tNota parcial 2: {estudiante.curso[2]} \n\tNota Final: {estudiante.curso[3]}");
+					
+					Console.WriteLine("-----------------------------------------------------------");						
+				}
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			
+			Console.ReadKey();
+        }
+    }
+}
+
